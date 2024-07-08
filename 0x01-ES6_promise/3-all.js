28b3c3b1e31809d228053ca747c5878f3bdf652e
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-07-08 19:57:57 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((data) => {
       console.log(`${data[0].body} ${data[1].firstName} ${data[2].lastName}`);
-    });
+    })
     .catch(() => console.log('Signup system offline'));
 }
 
