57da46d24f8fa21231b28f92223e2e436c753adb
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-07-08 20:34:12 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,5 +7,5 @@ function handleProfileSignup() {
     })
     .catch(() => console.log('Signup system offline'));
 }
-
+const isTestFn = node.parent?.type !== _utils.AST_NODE_TYPES.CallExpression || (0, _utils2.isTypeOfJestFnCall)(node.parent, context, ['test']);
 export default handleProfileSignup;
