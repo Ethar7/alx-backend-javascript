923a1d72d74da6d75304a08b4dc25d8bdc983f5c
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-07-08 20:11:57 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { createUser, uploadPhoto } from './utils'
+import { uploadPhoto,createUser } from './utils.js'
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
