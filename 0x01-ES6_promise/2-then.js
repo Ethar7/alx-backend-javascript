db4b6bce6593fb0c5747a5e5bc82f23a8d87d1b8
Ethar7 <nabihethar@yahoo.com>
2024-07-08 11:54:41 +0000

    p

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+function handleResponseFromAPI(promise) {
+  const state = { status: 200, body: 'success' };
+  
+  return promise
+    .then(() => state)
+    .catch((err) => err)
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
