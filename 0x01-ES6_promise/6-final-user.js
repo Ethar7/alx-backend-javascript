eb49cc9d8c357e25003e7cdbd639a6bc4f23887c
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-07-11 06:00:27 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,12 +5,12 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises).then((values) => {
     const result = [];
-  values.forEach((element) => {
-    if (element.status === 'fulfilled') {
-      result.push({ status: element.status, value: element.value });
-    } else {
-      result.push({ status: element.status, value: `${element.reason}` });
-    }
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
     });
   return result;
   });
