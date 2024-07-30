26c1909dd92e5a7bc589c21fc912d87fd62a690b
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-07-30 21:45:42 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,7 @@
 export default function createIteratorObject(report) {
 let arr = [];
-  for (const val of Object.val(report.allEmployees) {
+  for (const val of Object.val(report.allEmployees)) {
      arr = [...arr, ...val];
   }
+  return arr;
 }
