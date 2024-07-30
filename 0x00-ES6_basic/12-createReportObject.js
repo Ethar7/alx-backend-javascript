82b7fbe5b4c76fcffcd5f37d826d5693dd620aaa
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-07-30 21:29:15 +0300

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,4 +2,10 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: {
       ...employeesList,
+    },
+    getNumberOfDepartments(employeesList)
+    {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
