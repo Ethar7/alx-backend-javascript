3d14eb08791c02006f4b038757b4667e6153bb7a
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-14 08:15:08 +0300

    Create 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
+      throw new Error('invalid input type');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(val) {
+    if (typeof name !== 'string') {
+      throw new Error('invalid input type');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+  set length(val) {
+    if (typeof length !== 'number') {
+      throw new Error('invalid input type');
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+  set name(val) {
+    if (!Array.isArray(students)) {
+      throw new Error('invalid input type');
+    }
+    this._students = val;
+  }
+}
