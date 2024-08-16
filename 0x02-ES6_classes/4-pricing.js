8ecabfbfc012fc2ff3200479b40201c16aed4477
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-16 13:30:25 +0300

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
+    return `${this._amount} ${new Currency(this._currency.code, this._currency.name)}`;
   }
 
   static convertPrice(amount, conversionRate) {
