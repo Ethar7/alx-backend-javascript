40c8a6da50440415d6e611e2a46eb1941be248c0
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-16 15:30:17 +0300

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -32,3 +32,16 @@ export class StudentHolberton {
     return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
   }
 }
+
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+export const listOfStudents = [student1, student2, student3, student4, student5];
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+    student => student.fullStudentDescription
+);
