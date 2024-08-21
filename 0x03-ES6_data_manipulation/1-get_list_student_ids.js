d99871a74ffc5f1f7790448e90f56cd525d1db23
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-21 09:30:47 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,5 @@ export default function getListStudentIds(arr) {
   if (!Array.isarray(arr)) {
     return [];
   }
-  else {
-    return arr.map((student) => student.id);
-  }
+  return arr.map((student) => student.id);
 }
