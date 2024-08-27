9f0568a48af95ada0fac68fbe5e2259cfa4273f0
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-27 23:35:58 +0300

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,9 @@
-
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const Buf = new ArrayBuffer(length);
+  const int8 = new Int8Array(Buf);
+  int8.set(position, value);
+  return int8;
+}
