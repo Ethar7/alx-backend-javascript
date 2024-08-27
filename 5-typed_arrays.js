8432984363b7d67f98c5d5b0d44ddf64c449265c
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-27 23:37:07 +0300

    Delete 5-typed_arrays.js

--- 5-typed_arrays.js
+++
@@ -1,9 +0,0 @@
-export default function createInt8TypedArray(length, position, value) {
-  if (position >= length) {
-    throw new Error('Position outside range');
-  }
-  const Buf = new ArrayBuffer(length);
-  const int8 = new Int8Array(Buf);
-  int8.set(position, value);
-  return int8;
-}
