5c7052f6365197188c8bc92c702c9d2d4d9e8ff9
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-31 15:29:15 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,2 +1,13 @@
-export default function cleanSet() {
+export default function cleanSet(set, startString) {
+  const final_str = [];
+    // Iterate over each value in the set
+  for (const value of set) {
+        // Check if the value starts with the specified startString
+    if (value.startswith(startString)) {
+            // Append the rest of the string (after startString) to the cleanedValues array
+      final_str.push(value.slice(startString.length));
+    } 
+    // Join the cleaned values using hyphens and return the resulting string
+  }
+return final_str.join('-');
 }
