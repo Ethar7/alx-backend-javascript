4eb7dd5b671b2b0071841e11fe010bd4589b72a3
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-31 15:37:14 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,15 @@
 export default function cleanSet(set, startString) {
-    const finalStr = []; // Renamed variable to follow camel case
+  const finalStr = []; // Renamed variable to follow camel case
 
     // Iterate over each value in the set
-    for (const value of set) {
+  for (const value of set) {
         // Check if the value starts with the specified startString
-        if (value.startsWith(startString)) { // Fixed method name
+    if (value.startsWith(startString)) { // Fixed method name
             // Append the rest of the string (after startString) to the cleanedValues array
-            finalStr.push(value.slice(startString.length));
-        }
+      finalStr.push(value.slice(startString.length));
     }
+  }
 
     // Join the cleaned values using hyphens and return the resulting string
-    return finalStr.join('-');
+  return finalStr.join('-');
 }
