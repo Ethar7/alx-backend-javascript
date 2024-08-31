bcce73acdb7c5d38e00e149816a3a6608fa8b7b6
Ethar7 <125636422+Ethar7@users.noreply.github.com>
2024-08-31 12:57:47 +0300

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,8 @@
 export default function hasValuesFromArray(set, arr) {
   for (const i of arr) {
-    if (set.has(i)) {
-      return true;
+    if (!set.has(i)) {
+      return false;
     }
   }
-  return false;
+  return true;
 }
