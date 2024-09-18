24f2da6e57731f7dfde7c91f2f4625d212d8180e
Ethar7 <nabihethar@yahoo.com>
2024-09-18 06:34:16 +0000

    this

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * prints in STDOUT the string argument.
+ * @param {String} dispaly as message.
+ */
+const displayMessage = (param) => {
+  console.log(param);
+};
+
+module.exports = displayMessage;
